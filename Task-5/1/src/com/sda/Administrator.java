38056fb92c7e267c9c50e5e5b0e482338957aa5b
shcherbakov_dmitrii <best14sda@gmail.com>
2019-11-24 15:34:43 +0300

    Task5/1 some fix

--- Task-5/1/src/com/sda/Administrator.java
+++ Task-5/1/src/com/sda/Administrator.java
@@ -2,6 +2,7 @@ package com.sda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class Administrator {
@@ -57,10 +58,10 @@ public class Administrator {
     public void setRegistration(int guestIDcard, int roomNumber) {
         Guest guest = guestList.stream().filter(a -> a.getIdCardNumber() == guestIDcard).findFirst().orElse(null);
         Room room = roomList.stream().filter(a -> a.getNumber() == roomNumber).findFirst().orElse(null);
-        if (Optional.ofNullable(room).isPresent() && Optional.ofNullable(guest).isPresent()) {
+        Optional.ofNullable(guest).ifPresent(g -> Optional.ofNullable(room).ifPresent(r -> {
             room.setBusy(true);
             guest.setRoom(room.getNumber());
-        }
+        }));
     }
 
     public void setServiceToGuest(int guestIdCard, String serviceName) {
@@ -68,18 +69,19 @@ public class Administrator {
                 .filter(a -> a.getIdCardNumber() == guestIdCard).findFirst().orElse(null);
         Service service = serviceList.stream()
                 .filter(a -> a.getServiceName().equals(serviceName)).findFirst().orElse(null);
-        if (Optional.ofNullable(service).isPresent() && Optional.ofNullable(guest).isPresent()) {
-            guest.setChooseServices(service);
-        }
+        Optional.ofNullable(guest).ifPresent(g -> Optional.ofNullable(service)
+                .ifPresent(s -> guest.setChooseServices(service)));
+
     }
 
     public void cancelRegistration(int guestIdCard) {
         Guest guest = guestList.stream().filter(a -> a.getIdCardNumber() == guestIdCard).findFirst().orElse(null);
         Room room = roomList.stream().filter(a -> a.getNumber() == guest.getRoom()).findFirst().orElse(null);
-        if (Optional.ofNullable(room).isPresent() && Optional.ofNullable(guest).isPresent()) {
+        Optional.ofNullable(room).ifPresent(r -> Optional.ofNullable(guest).ifPresent(g -> {
             room.setBusy(false);
             guest.setRoom(null);
-        }
+        }));
+
     }
 
     public Service addService(String name, int cost, String description) {
@@ -95,40 +97,39 @@ public class Administrator {
     }
 
     public void closeService(String name) {
-        serviceList.remove(serviceList.stream()
-                .filter(a -> a.getServiceName().equals(name)).findFirst().orElse(null));
+        serviceList.remove(serviceList.stream().filter(Objects::nonNull)
+                .filter(a -> a.getServiceName().equals(name)).findFirst().get());
     }
 
     public void changeCost(int roomNumber, int newCost) {
-        Room room = roomList.stream().filter(a -> a.getNumber() == roomNumber).findFirst().orElse(null);
-        Optional.ofNullable(room).ifPresent(r -> r.setCost(newCost));
+        roomList.stream().filter(Objects::nonNull)
+                .filter(a -> a.getNumber() == roomNumber).findFirst().ifPresent(r -> r.setCost(newCost));
     }
 
     public void changeCost(String serviceName, int newCost) {
-        Service service = serviceList.stream().filter(a -> a.getServiceName().equals(serviceName))
-                .findFirst().orElse(null);
-        Optional.ofNullable(service).ifPresent(s -> s.setCost(newCost));
+        serviceList.stream().filter(Objects::nonNull).filter(a -> a.getServiceName().equals(serviceName))
+                .findFirst().ifPresent(s -> s.setCost(newCost));
+
     }
 
     public void printRoomList() {
         System.out.println("Rooms:");
-        for (Room room : roomList) {
-            if (Optional.ofNullable(room).isPresent()) {
-                System.out.println("Room #" + room.getNumber() + " is " + (room.getBusy() ? " ready" : " busy"));
-            }
-        }
+        roomList.forEach(r -> Optional.ofNullable(r)
+                .ifPresent(room -> System.out.println("Room #" + room.getNumber() + " is "
+                        + (room.getBusy() ? " ready" : " busy"))));
+
     }
 
     public void printGuestList() {
         System.out.println("Guests:");
-        for (Guest guest : guestList) {
-            if (Optional.ofNullable(guest).isPresent()) {
-                System.out.println(guest.getName() + " " + guest.getSurname() +
-                        (guest.getRoom() == null ? " not registred" : " in room " + guest.getRoom())
-                        + " choose services: " + guest.getCountServices());
-                guest.getChooseServices();
-            }
-        }
+        guestList.forEach(g -> Optional.ofNullable(g).ifPresent(guest -> {
+            System.out.println(guest.getName()
+                    + " " + guest.getSurname()
+                    + (guest.getRoom() == null ? " not registred" : " in room " + guest.getRoom())
+                    + " choose services: " + guest.getCountServices());
+            g.getChooseServices();
+
+        }));
     }
 
     // Task5-1 посчитать количество заселенных/свободных комнат
